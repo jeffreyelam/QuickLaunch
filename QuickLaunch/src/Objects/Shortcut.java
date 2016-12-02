7ce331fa036778486e08a08432509147b13881fe
Jeffrey Elam <Jeffrey.elam55@gmail.com>
2016-12-01 19:45:34 -0500

    Adding Constructor

--- QuickLaunch/src/Objects/Shortcut.java
+++ QuickLaunch/src/Objects/Shortcut.java
@@ -1,5 +1,15 @@
 package Objects;
 
-public class Shortcut {
-
+public class Shortcut
+{
+	private String fileShortcut;
+	private String filePath;
+	private boolean isEnvironmentBased;
+	
+	public Shortcut(String shortcut, String path, boolean environmentBased)
+	{
+		this.fileShortcut = shortcut;
+		this.filePath = path;
+		this.isEnvironmentBased = environmentBased;
+	}
 }
