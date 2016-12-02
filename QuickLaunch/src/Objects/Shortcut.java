b266219ba72d7cf55aa2b2e1fb7fcae41c84c9b7
Jeffrey Elam <Jeffrey.elam55@gmail.com>
2016-12-01 20:35:28 -0500

    Implementing ShortcutDirectory as Hashmap

--- QuickLaunch/src/Objects/Shortcut.java
+++ QuickLaunch/src/Objects/Shortcut.java
@@ -2,13 +2,11 @@ package Objects;
 
 public class Shortcut
 {
-	private String fileShortcut;
 	private String filePath;
 	private boolean isEnvironmentBased;
 	
-	public Shortcut(String shortcut, String path, boolean environmentBased)
+	public Shortcut(String path, boolean environmentBased)
 	{
-		this.fileShortcut = shortcut;
 		this.filePath = path;
 		this.isEnvironmentBased = environmentBased;
 	}
@@ -16,11 +14,6 @@ public class Shortcut
 	public String getFilePath()
 	{
 		return this.filePath;
-	}
-	
-	public String getFileShortcut()
-	{
-		return this.fileShortcut;
 	}
 	
 	public boolean getEnviromentBased()
@@ -31,11 +24,6 @@ public class Shortcut
 	public void updateFilePath(String path)
 	{
 		this.filePath = path;
-	}
-	
-	public void updateFileShortcut(String shortcut)
-	{
-		this.fileShortcut = shortcut;
 	}
 	
 	public void updateEnvironmentBased(boolean environmentBased)
--- QuickLaunch/src/Objects/ShortcutDirectory.java
+++ QuickLaunch/src/Objects/ShortcutDirectory.java
@@ -1,5 +1,12 @@
 package Objects;
 
-public class ShortcutDirectory {
+import java.util.HashMap;
 
+@SuppressWarnings({ "serial", "hiding" })
+public class ShortcutDirectory<String,Shortcut> extends HashMap<String,Shortcut>
+{
+	public ShortcutDirectory()
+	{
+		super();
+	}
 }
