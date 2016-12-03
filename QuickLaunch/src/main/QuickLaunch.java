f920bdf828820b1bcdbd6de610c404c5df6830a2
Jeffrey Elam <Jeffrey.elam55@gmail.com>
2016-12-02 22:29:42 -0500

    Creating the GUI layout and adding inner class to allow the JFrame to be dragged, even without the Window Options

---
+++ QuickLaunch/src/main/QuickLaunch.java
@@ -0,0 +1,11 @@
+package main;
+
+import userinterfaces.Launcher;
+
+public class QuickLaunch
+{
+	public static void main(String[] args)
+	{
+		Launcher shortcutLauncher = new Launcher();
+	}
+}
--- QuickLaunch/src/userinterfaces/Launcher.java
+++ QuickLaunch/src/userinterfaces/Launcher.java
@@ -1,5 +1,68 @@
 package userinterfaces;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-public class Launcher {
+@SuppressWarnings("serial")
+public class Launcher extends JFrame
+{
+	public Launcher()
+	{
+		this.setUndecorated(true);
 
+		//Create text box
+		JTextField input = new JTextField();
+		Font font = new Font("SansSerif", Font.BOLD, 20);
+		input.setFont(font);
+        input.setPreferredSize( new Dimension( 150, 30 ) );
+        
+        JLabel envLabel = new JLabel("STAGING  ");
+        
+        //Create panel to hold the input box and label
+        JPanel contentPane = new JPanel();
+        contentPane.setBackground(Color.BLACK);
+        contentPane.setPreferredSize(new Dimension(225, 40));
+        contentPane.add(envLabel);
+        contentPane.add(input);
+        this.setContentPane(contentPane);
+
+        //Create drag listener that moves the JFrame
+        FrameDragListener frameDragListener = new FrameDragListener(this);
+        this.addMouseListener(frameDragListener);
+        this.addMouseMotionListener(frameDragListener);
+
+        //Show this JFrame        
+        this.pack();
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+	}
+        
+    public static class FrameDragListener extends MouseAdapter
+    {
+
+        private final JFrame frame;
+        private Point mouseDownCompCoords = null;
+
+        public FrameDragListener(JFrame frame)
+        {
+            this.frame = frame;
+        }
+
+        public void mouseReleased(MouseEvent e)
+        {
+            mouseDownCompCoords = null;
+        }
+
+        public void mousePressed(MouseEvent e)
+        {
+            mouseDownCompCoords = e.getPoint();
+        }
+
+        public void mouseDragged(MouseEvent e)
+        {
+            Point currCoords = e.getLocationOnScreen();
+            frame.setLocation(currCoords.x - mouseDownCompCoords.x, currCoords.y - mouseDownCompCoords.y);
+        }
+    }
 }
